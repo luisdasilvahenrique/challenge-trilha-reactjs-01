e7cd1a09d37a27ce1cb7221e7f867616174214b6
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-03-02 00:31:42 -0300

    to-do add new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,11 +16,25 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if(!newTaskTitle) return;
+
+    const newTask = {
+    id: Math.random(),
+    title: newTaskTitle,
+    isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
   }
+
+  
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
