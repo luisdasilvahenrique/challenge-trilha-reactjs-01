ce09367ffc5e65c41a86ef2ff86aa2f4c135c44b
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-03-02 00:54:42 -0300

    to-do changing checkbox: taskComplete for true or false

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -31,16 +31,21 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const filteredTasks = tasks.filter(task => task.id !== id)
-
-    setTasks(filteredTasks);
-
+    const taskComplete = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+    
+    setTasks(taskComplete)
   }
 
   
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(filteredTasks);
   }
 
   return (
